324f79c499f37964d81aaa5942b7e0b09ede41f2
kennygeiler <kennethlgeiler@gmail.com>
2015-05-08 18:42:06 -0400

    update parcel factory attributes

--- spec/factories/parcels.rb
+++ spec/factories/parcels.rb
@@ -6,7 +6,7 @@ FactoryGirl.define do
     pickup_by Date.parse('1995-03-03')
     deliver_by Date.parse('1995-04-04')
     origin_address_id 1
-    destination_address_id 3
+    destination_address_id 2
     sender_id 1
     delivery_notes "fragile"
   end
