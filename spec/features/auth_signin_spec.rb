64f98b042d709f877db03f851bb18cc0790e6928
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-08 12:11:39 -0400

    Add features test for user signing in on auth_signin_spec

--- spec/features/auth_signin_spec.rb
+++ spec/features/auth_signin_spec.rb
@@ -1,9 +1,22 @@
 require 'spec_helper'
 feature "Auth" do
-  scenario "A user can access the signin page" do
-    visit signin_path
-    expect(page).to have_content("Sign In")
-    expect(page).to have_content("Name")
-    expect(page).to have_content("Password")
+  context "signin" do
+    scenario "A user can access the signin page" do
+      visit signin_path
+      expect(page).to have_content("Sign In")
+      expect(page).to have_content("Name")
+      expect(page).to have_content("Password")
+    end
+    scenario "A successful sign in redirects to the user profile" do
+      User.create(name: "test_user",
+                  password: "password",
+                  password_confirmation: "password")
+      visit signin_path
+      fill_in "session[name]", :with => "test_user"
+      fill_in "session[password]", :with => "password"
+      click_on "login"
+      expect(page).to have_content("test_user")
+    end
+
   end
 end
