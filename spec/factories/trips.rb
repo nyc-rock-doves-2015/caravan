df59881ed3ca3142826d62fefec87c2d5691cd4b
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-08 16:56:46 -0400

    Add trip factory

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -1,6 +1,16 @@
 FactoryGirl.define do
   factory :trip do
-    
+  leaving_at Faker::Date.forward(3)
+  arriving_at Faker::Date.forward(10)
+  available_volume rand(10..400)
+  max_weight rand(1..200)
+  rate Faker::Commerce.price
+  content_restrictions Faker::Lorem.paragraph(2)
+  vehicle "U-Haul"
+
+  association :origin_address, factory: :address
+  association :destination_address, factory: :address
+  association :driver, factory: :user
   end
 
 end
