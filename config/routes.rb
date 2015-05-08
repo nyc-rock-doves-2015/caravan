2c08f4e9a1251e6b221a5dede7739ae83c16802c
Charles Green <Charles.Green88@gmail.com>
2015-05-08 10:13:05 -0400

    rebased master into dev

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,13 @@ Rails.application.routes.draw do
 
   resource :session, only: [:new, :create, :destroy]
   resources :users
-<<<<<<< HEAD
-=======
-  get '/profiles/:id', to: redirect('/users/%{id}')
->>>>>>> Bootstrap working, functional user and profile route to user show
 
   get '/profiles/:id', to: redirect('/users/%{id}')
   get 'signin' => 'sessions#new'
   post 'signin' => 'sessions#create'
   get 'signout' => 'sessions#destroy'
   get 'signup' => 'users#new'
+  
   resources :parcels do
     resources :trips
   end
@@ -20,8 +17,5 @@ Rails.application.routes.draw do
     resources :parcels
   end
 
-<<<<<<< HEAD
  root 'users#show'
-=======
->>>>>>> Bootstrap working, functional user and profile route to user show
 end
