6270d1a9be9ae95b9fd9515dd11aa9a6729a0dc6
Charles Green <Charles.Green88@gmail.com>
2015-05-08 11:59:49 -0400

    removed comments

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,6 @@ RSpec.describe User, type: :model do
 	let!(:baduser) {User.new(username:"", password:"")}
   pending "add some examples to (or delete) #{__FILE__}"
 
-# test for creation
-
 	let(:user) {FactoryGirl.build :user}
 	it "should have a username and password" do
 		expect(user.username).to eq "Bob"
@@ -15,4 +13,5 @@ RSpec.describe User, type: :model do
 	it "should not create a user with bad params" do
 		expect(baduser.save).to eq false
 	end
+
 end
