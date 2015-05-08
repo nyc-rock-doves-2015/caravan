bd827a0bc457e02cc95d9e8b156f846f232ce027
bicyclethief <bicyclethief@gmail.com>
2015-05-08 15:43:51 -0400

    Add tests for Address model

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.build :user }
+  let(:address) { FactoryGirl.build :address }
+
+  it "is valid with a user_id, city, state, zip code" do
+    address = Address.new(user_id: user.id,
+                          city: Faker::Address.city,
+                          state: Faker::Address.state_abbr,
+                          zip_code: Faker::Address.zip_code)
+    expect(address).to be_valid
+  end
+
+  it "is invalid without a user_id" do
+    address = Address.new(city: Faker::Address.city,
+                          state: Faker::Address.state_abbr,
+                          zip_code: Faker::Address.zip_code)
+    expect(address).to be_invalid
+  end
+
+  it "is invalid without a city" do
+    address = Address.new(user: user,
+                          state: Faker::Address.state_abbr,
+                          zip_code: Faker::Address.zip_code)
+    expect(address).to be_invalid
+  end
+
+  it "is invalid without a state" do
+    address = Address.new(user: user,
+                          city: Faker::Address.city,
+                          zip_code: Faker::Address.zip_code)
+    expect(address).to be_invalid
+  end
+
+  it "is invalid with a state longer than 2 characters" do
+    address = Address.new(user: user,
+                          city: Faker::Address.city,
+                          state: "USA",
+                          zip_code: Faker::Address.zip_code)
+    expect(address).to be_invalid
+  end
+
+  it "is invalid without a zipcode" do
+    address = Address.new(user: user,
+                          city: Faker::Address.city,
+                          state: Faker::Address.state_abbr)
+    expect(address).to be_invalid
+  end
 end
