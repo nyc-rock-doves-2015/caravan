e4243a305fe375b65bac4406b7f46f89ee1bc07f
bicyclethief <bicyclethief@gmail.com>
2015-05-07 20:14:09 -0400

    Add association between an address and its creator (user)

--- db/migrate/20150507202700_create_addresses.rb
+++ db/migrate/20150507202700_create_addresses.rb
@@ -1,6 +1,7 @@
 class CreateAddresses < ActiveRecord::Migration
   def change
     create_table :addresses do |t|
+      t.references :user, null: false
       t.string :description
       t.string :street_address
       t.string :secondary_address
