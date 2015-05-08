f86144855f9bc1614573e4faa7d2ac399ff93029
kennygeiler <kennethlgeiler@gmail.com>
2015-05-08 14:50:20 -0400

    inital setup for parcel controller tests

--- spec/controllers/parcels_controller_spec.rb
+++ spec/controllers/parcels_controller_spec.rb
@@ -2,4 +2,29 @@ require 'rails_helper'
 
 RSpec.describe ParcelsController, type: :controller do
 
+  let(:user) { FactoryGirl.create :user }
+  let(:parcel1) { FactoryGirl.create :parcel }
+  let(:parcel2) { FactoryGirl.build :parcel }
+  let(:parcel3) { Parcel.new(description:"blah") }
+
+  context "#create" do
+    it "redirects to the users profile" do
+      post :create, parcel: FactoryGirl.attributes_for(:parcel),
+        expect(response).to redirect_to user_path(User.last.id)
+    end
+
+    it "should not redirect with bad attributes" do
+      post :create, user: {username: nil , password: "wdw"}
+      expect(response).to redirect_to new_user_path
+    end
+
+    it "creates a post with valid params" do
+      post :create
+      post2.save
+      expect(post2).to eq Post.last
+    end
+
+  end
+
+
 end
--- spec/factories/parcels.rb
+++ spec/factories/parcels.rb
@@ -1,6 +1,15 @@
 FactoryGirl.define do
   factory :parcel do
-    
+    weight 4
+    volume 3
+    description "pet cat"
+    pickup_by 03/03/1995
+    deliver_by 04/04/1995
+    origin_address_id 1
+    destination_address_id 3
+    sender_id 1
+    trip_id 2
+    delivery_notes "fragile"
   end
 
 end
