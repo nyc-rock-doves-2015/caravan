94a312f6f55952167ba2cdf296feaca7b8c183a3
bicyclethief <bicyclethief@gmail.com>
2015-05-07 23:42:58 -0400

    Add basic functionality to create trip

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,28 @@
+class TripsController < ActionController::Base
+
+  def create
+    trip = Trip.build(origin_address_params, destination_address_params, trip_params)
+
+    if trip && trip.id
+      redirect_to profile_path
+    else
+      flash[:error] = trip.errors.full_messages.join('<br>')
+      # TODO: recycle params so user does not have to re-input
+      render :new
+    end
+  end
+
+  private
+
+  def origin_address_params
+    params.require(:origin_address).permit(:user_id, :description, :street_address, :secondary_address, :city, :state, :zip_code)
+  end
+
+  def destination_address_params
+    params.require(:destination_address).permit(:user_id, :description, :street_address, :secondary_address, :city, :state, :zip_code)
+  end
+
+  def trip_params
+    params.require(:trip).permit(:driver_id, :leaving_at, :arriving_at, :available_volume, :max_weight, :rate, :content_restrictions, :vehicle)
+  end
+end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,30 @@ class Trip < ActiveRecord::Base
   has_many :parcels
   belongs_to :origin_address, class_name: "Address"
   belongs_to :destination_address, class_name: "Address"
+
+  def self.build(origin_address_params, destination_address_params, trip_params)
+    origin_address = Address.new(origin_address_params)
+    unless origin_address.save
+      return origin_address
+    end
+
+    destination_address = Address.new(destination_address_params)
+    unless destination_address.save
+      origin_address.destroy
+      return destination_address
+    end
+
+    trip = Trip.new(trip_params)
+    trip.origin_address = origin_address
+    trip.destination_address = destination_address
+
+    if trip.save
+      return trip
+    else
+      origin_address.destroy
+      destination_address.destroy
+      trip.destroy
+      return trip
+    end
+  end
 end
