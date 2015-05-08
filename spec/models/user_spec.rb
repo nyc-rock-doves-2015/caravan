b5d84271655fdd6ceda3479daf0a7bc94c4747da
Charles Green <Charles.Green88@gmail.com>
2015-05-08 11:04:04 -0400

    add user test for create

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+# test for creation
+
+	let(:user) {FactoryGirl.build :user}
+	it "should have a username and password" do
+		expect(user.username).to eq "Bob"
+		expect(user.password).to eq "password"
+	end
+
 end
