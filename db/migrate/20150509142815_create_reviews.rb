b8d3f7d1e5c6c44951f5bd81def35b348a56480b
kennygeiler <kennethlgeiler@gmail.com>
2015-05-09 11:32:29 -0400

    edit review migration

--- db/migrate/20150509142815_create_reviews.rb
+++ db/migrate/20150509142815_create_reviews.rb
@@ -1,8 +1,6 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
-      t.references :driver
-      t.references :sender
       t.references :trip
       t.references :parcel
       t.integer :rating
