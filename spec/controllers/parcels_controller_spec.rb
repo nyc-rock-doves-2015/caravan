f0d542d864948a6292e5795c42b9be8e6b46ee4e
bicyclethief <bicyclethief@gmail.com>
2015-05-09 17:06:57 -0400

    Stub authenticated user to pass broken tests

--- spec/controllers/parcels_controller_spec.rb
+++ spec/controllers/parcels_controller_spec.rb
@@ -1,23 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe ParcelsController, type: :controller do
+  let(:user) { FactoryGirl.create :user }
   before :each do
-    @user = FactoryGirl.create(:user)
+    stub_current_user(user)
+    stub_authorize_user!
   end
 
   context "#create" do
     it "redirects to the users profile" do
       p_params = FactoryGirl.attributes_for(:parcel)
-      o_params = FactoryGirl.attributes_for(:address, user_id: @user.id)
-      d_params = FactoryGirl.attributes_for(:address, user_id:@user.id)
+      o_params = FactoryGirl.attributes_for(:address, user_id: user.id)
+      d_params = FactoryGirl.attributes_for(:address, user_id: user.id)
       expect {post :create, origin_address:o_params, destination_address:d_params, parcel:p_params}.to change{Parcel.count}.by(1)
       expect(response).to redirect_to profile_path
     end
 
     it "creates a parcels with valid params" do
       p_params = FactoryGirl.attributes_for(:parcel)
-      o_params = FactoryGirl.attributes_for(:address, user_id: @user.id)
-      d_params = FactoryGirl.attributes_for(:address, user_id:@user.id)
+      o_params = FactoryGirl.attributes_for(:address, user_id: user.id)
+      d_params = FactoryGirl.attributes_for(:address, user_id: user.id)
       expect {post :create, origin_address:o_params, destination_address:d_params, parcel:p_params}.to change{Parcel.count}.by(1)
     end
   end
@@ -37,16 +39,16 @@ RSpec.describe ParcelsController, type: :controller do
   context "#edit" do
     before :each do
       p_params = FactoryGirl.attributes_for(:parcel)
-      o_params = FactoryGirl.attributes_for(:address, user_id: @user.id)
-      d_params = FactoryGirl.attributes_for(:address, user_id:@user.id)
+      o_params = FactoryGirl.attributes_for(:address, user_id: user.id)
+      d_params = FactoryGirl.attributes_for(:address, user_id: user.id)
       post :create, origin_address:o_params, destination_address:d_params, parcel:p_params
       @parcel = Parcel.last
       user = FactoryGirl.create(:user)
       allow(controller).to receive(:current_user) { user}
-
     end
+
     it "updates a parcel" do
-      put :update, id: @parcel, parcel: FactoryGirl.attributes_for(:parcel, delivery_notes: 'yo'), origin_address: FactoryGirl.attributes_for(:address, user_id: @user.id), destination_address: FactoryGirl.attributes_for(:address, user_id: @user.id)
+      put :update, id: @parcel, parcel: FactoryGirl.attributes_for(:parcel, delivery_notes: 'yo'), origin_address: FactoryGirl.attributes_for(:address, user_id: user.id), destination_address: FactoryGirl.attributes_for(:address, user_id: user.id)
       @parcel.reload
       expect(@parcel.delivery_notes).to eq('yo')
     end
