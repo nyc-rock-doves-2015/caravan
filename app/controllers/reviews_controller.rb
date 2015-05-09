00b07c5993cb36742d4392babf35cb4741a51ff5
kennygeiler <kennethlgeiler@gmail.com>
2015-05-09 12:15:58 -0400

    add create and delete review methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,29 @@
 class ReviewsController < ApplicationController
+
+  def new
+    session[:return_to] ||= request.referer
+  end
+
+  def create
+    @review = Review.create(review_params)
+    if @review.save
+      redirect_to session.delete(:return_to)
+    else
+      flash[:error] = trip.errors.full_messages.join('<br>')
+      render :new
+    end
+  end
+
+  def destroy
+    review = Review.find(params[:id])
+    review.destroy
+    redirect_to current_user_profile
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:trip_id, :parcel_id, :rating, :content)
+  end
+
 end
