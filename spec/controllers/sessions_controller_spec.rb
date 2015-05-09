97114a80f6f1c44476e4faed36fb88da22c1e754
Charles Green <Charles.Green88@gmail.com>
2015-05-09 16:41:22 -0400

    add sessions controller spec skeleton

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  let(:user) {FactoryGirl.build :user}
+  # test create
+  describe "GET #new" do
+    it "should render the signin form" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+
+  describe "POST #create" do
+    it "creates a new session when a user signs in" do
+      post :create, user: FactoryGirl.attributes_for(:user)
+      expect(response).to redirect_to user_path(User.last.id)
+    end
+    it "renders the user's profile"
+  end
+
+  describe "get destroy" do
+    it "should clear the session"
+    it "should redirect to the index page"
+  end
 
 end
