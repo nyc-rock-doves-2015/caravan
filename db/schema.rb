9162dae7d558a66c8785955e5561347526eb4830
kennygeiler <kennethlgeiler@gmail.com>
2015-05-09 11:32:51 -0400

    up to date schema of current models and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150507202700) do
+ActiveRecord::Schema.define(version: 20150509142815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,15 @@ ActiveRecord::Schema.define(version: 20150507202700) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "trip_id"
+    t.integer  "parcel_id"
+    t.integer  "rating"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "trips", force: :cascade do |t|
