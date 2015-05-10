ebea50fd140dab14da84f7c183185933c992f77d
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-10 15:05:40 -0400

    Add received reviews association to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,9 @@ class User < ActiveRecord::Base
   has_many :parcels, foreign_key: :sender_id
 
   has_many :reviews, foreign_key: :reviewer_id
-  has_many :reviews, foreign_key: :reviewee_id
+  has_many :received_reviews, class_name: "Review", foreign_key: :reviewee_id
   # has_many :driver_reviews, class_name: "Review", through: :trips, source: :driver
   # has_many :sender_reviews, class_name: "Review", through: :parcels, source: :sender
-
 
   validates :username, presence: true
 
