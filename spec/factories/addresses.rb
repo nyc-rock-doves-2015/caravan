a1d720ee063216ee50b775ed944403c73a4e2775
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-10 12:05:51 -0400

    Test model tests, testing build, all_matching_parcel, match_reviewer methods

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -2,8 +2,8 @@ FactoryGirl.define do
   factory :address do
     sequence(:id) { |n| n }
     association :user, factory: :user
-    city Faker::Address.city
-    state Faker::Address.state_abbr
+    city 'Portland'
+    state 'OR'
     zip_code Faker::Address.zip_code
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :user do
+    sequence(:id) { |n| n }
     username "Bob"
     password "123"
   end
