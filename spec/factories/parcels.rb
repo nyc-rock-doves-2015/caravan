7150a9ac477049e9d836691fec386463f5580e6c
bicyclethief <bicyclethief@gmail.com>
2015-05-09 22:50:46 -0400

    Use factory girl associations for addresses

--- spec/factories/parcels.rb
+++ spec/factories/parcels.rb
@@ -5,9 +5,9 @@ FactoryGirl.define do
     description "pet cat"
     pickup_by Date.parse('1995-03-03')
     deliver_by Date.parse('1995-04-04')
-    origin_address_id 1
-    destination_address_id 2
     sender_id 1
     delivery_notes "fragile"
+    association :destination_address, factory: :address
+    association :origin_address, factory: :address
   end
 end
