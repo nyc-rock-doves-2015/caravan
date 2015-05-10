7cb41e5483cafe5e2f886a30c2fc7b188790a8d6
bicyclethief <bicyclethief@gmail.com>
2015-05-09 23:47:56 -0400

    Add tests for #create and #destroy

--- spec/controllers/trips_controller_spec.rb
+++ spec/controllers/trips_controller_spec.rb
@@ -41,4 +41,33 @@ RSpec.describe TripsController, type: :controller do
       expect(assigns(:parcel)).to eq parcel
     end
   end
+
+  describe "POST #create" do
+    it "creates a trip with valid params" do
+      t_params = FactoryGirl.attributes_for(:trip)
+      o_params = FactoryGirl.attributes_for(:address, user_id: user.id)
+      d_params = FactoryGirl.attributes_for(:address, user_id: user.id)
+      expect {post :create, origin_address:o_params, destination_address:d_params, trip:t_params}.to change{Trip.count}.by(1)
+    end
+
+    it "redirects to the user's profile" do
+      t_params = FactoryGirl.attributes_for(:trip)
+      o_params = FactoryGirl.attributes_for(:address, user_id: user.id)
+      d_params = FactoryGirl.attributes_for(:address, user_id: user.id)
+      expect {post :create, origin_address:o_params, destination_address:d_params, trip:t_params}.to change{Trip.count}.by(1)
+      expect(response).to redirect_to profile_path
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it "deletes a trip" do
+      trip = FactoryGirl.create(:trip)
+      user = FactoryGirl.create(:user)
+      allow(controller).to receive(:current_user) { user}
+
+      expect {
+        delete :destroy, id: trip
+      }.to change{Trip.count}.by(-1)
+    end
+  end
 end
