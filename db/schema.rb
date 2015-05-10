307eb01a8fa17572b349697f218d30eea241df9c
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-10 13:00:33 -0400

    Add reputation migration to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509142815) do
+ActiveRecord::Schema.define(version: 20150510165214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,12 +69,13 @@ ActiveRecord::Schema.define(version: 20150509142815) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",        limit: 50, null: false
-    t.string   "password_digest",            null: false
+    t.string   "username",        limit: 50,                         null: false
+    t.string   "password_digest",                                    null: false
     t.string   "email"
     t.string   "phone"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.decimal  "reputation",                 precision: 2, scale: 1
   end
 
 end
