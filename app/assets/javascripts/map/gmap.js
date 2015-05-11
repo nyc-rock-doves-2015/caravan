7e62a4d6650ff61ba67d198baf1a68f36e712c19
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-11 14:47:58 -0400

    Eliminate code for map, change location search id to origin search id

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -1,84 +1,16 @@
-GmapAutoComplete = function() {
-  //  var defaultZoom = 16; // Why zoomed in? Because it looks good.
-
-  // // create map and add controls
-  // var mapOptions = {
-  //   center: new google.maps.LatLng(13.731547730050778, 100.56798934936523),
-  //   zoom: defaultZoom,
-  //   mapTypeId: google.maps.MapTypeId.ROADMAP,
-  //   scrollwheel: false,
-  //   scaleControl: false
-  // };
-  // var map = new google.maps.Map(document.getElementById('googlemap'),mapOptions);
-
-  // // I'm Centering the map in Bangkok
-  // // '13.731547730050778'
-  // // '100.56798934936523'
-  // var mylat = document.getElementById("origin_address_latitude").value;
-  // var mylng = document.getElementById("origin_address_longitude").value;
-  // if (jQuery.isEmptyObject(mylat)) {
-  //   var mylat = '13.731548';
-  //   var mylng = '100.56799';
-  //   var defaultZoom = 14;
-  // };
-  // // set center point of map to be of the Marker or the Default City
-  // var centrePoint = new google.maps.LatLng(mylat, mylng);
-  // map.setCenter(centrePoint);
-  // map.setZoom(defaultZoom);
-
-  // // add a draggable marker
-  // marker = new google.maps.Marker({
-  //   map: map,
-  //   draggable: true,
-  //   animation: google.maps.Animation.DROP,
-  //   position: centrePoint
-  // });
-  // var defaultImage = new google.maps.MarkerImage(
-  //     "http://maps.gstatic.com/mapfiles/place_api/icons/geocode-71.png",
-  //     new google.maps.Size(71, 71),
-  //     new google.maps.Point(0, 0),
-  //     new google.maps.Point(17, 34),
-  //     new google.maps.Size(35, 35));
-  // marker.setIcon(defaultImage);
-
-  // google.maps.event.addListener(marker, 'click', toggleBounce);
-
-  // // create Info window for marker
-  // var infowindow = new google.maps.InfoWindow();
+GmapAutoComplete = function(firstSearch) {
   // Autocomplete
- var input = document.getElementById('location_search');
+ var input = document.getElementById('origin_search');
   var autocomplete = new google.maps.places.Autocomplete(input);
-  // autocomplete.bindTo('bounds', map);
-
-  // add a drag listener to the map
-  // google.maps.event.addListener(marker, "dragend", function() {
-  //   var point = marker.getPosition();
-  //   map.panTo(point);
-  //   document.getElementById("origin_address_latitude").value = point.lat();
-  //   document.getElementById("origin_address_longitude").value = point.lng();
-  //   infowindow.close();
-  //   marker.setIcon(defaultImage);
-  // });
   var place;
   var photos;
   // Autocomplete Listener
   google.maps.event.addListener(autocomplete, 'place_changed', function() {
     place = autocomplete.getPlace();
-    // marker.setPosition(place.geometry.location);
-    // map.panTo(place.geometry.location);
-    // map.setZoom(16);
+
     document.getElementById("origin_address_latitude").value = place.geometry.location.lat();
     document.getElementById("origin_address_longitude").value = place.geometry.location.lng();
 
-    // Sets the proper image on the marker. ie. school/hospital marker
-    // var image = new google.maps.MarkerImage(
-    //     place.icon,
-    //     new google.maps.Size(71, 71),
-    //     new google.maps.Point(0, 0),
-    //     new google.maps.Point(17, 34),
-    //     new google.maps.Size(35, 35));
-    // marker.setIcon(image);
-    // marker.setPosition(place.geometry.location);
     var address = '';
     if (place.address_components) {
       address = [(place.address_components[0] &&
@@ -89,9 +21,6 @@ GmapAutoComplete = function() {
                   place.address_components[2].short_name || '')
                 ].join(' ');
     }
-    // infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-    // infowindow.open(map, marker);
-
     // Autofill the fields in the form. TODO: improve parsing of the returned JSON data.
     var address = place.address_components;
     console.log(address)
@@ -130,8 +59,6 @@ GmapAutoComplete = function() {
     if (location_name) {
       document.getElementById("origin_address_street_address").value = location_name;
     }
-
-
   });
 
 }
--- app/assets/javascripts/wizard.js
+++ app/assets/javascripts/wizard.js
@@ -19,13 +19,14 @@ $(document).ready(function () {
             $target.find('input:eq(0)').focus();
         }
     });
-
+    var firstSearch = true;
     allNextBtn.click(function(){
         var curStep = $(this).closest(".setup-content"),
             curStepBtn = curStep.attr("id"),
             nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
             curInputs = curStep.find("input[type='text'],input[type='url']"),
             isValid = true;
+            firstSearch = false;
 
         $(".form-group").removeClass("has-error");
         for(var i=0; i<curInputs.length; i++){
@@ -37,9 +38,10 @@ $(document).ready(function () {
 
         if (isValid)
             nextStepWizard.removeAttr('disabled').trigger('click');
+
     });
 
     $('div.setup-panel div a.btn-primary').trigger('click');
 
-    var gmapAuto = GmapAutoComplete();
+    var gmapAuto = GmapAutoComplete(firstSearch);
 });
