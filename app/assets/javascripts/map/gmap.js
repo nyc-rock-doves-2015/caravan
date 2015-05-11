f0fd45e1988da022e3445e9fc21a4ac2e54b1126
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-11 09:38:29 -0400

    Add google places with search to javascript

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -8,71 +8,45 @@
 // })
 
 $(document).ready(function(){
- function initialize() {
-
-  var markers = [];
-  var map = new google.maps.Map(document.getElementById('map-canvas'), {
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+ var defaultZoom = 17; // Why zoomed in? Because it looks good.
+  var lat = <%= @location.lat %>;
+  var lng = <%= @location.lng %>;
+  // create map and add controls
+  var mapOptions = {
+    center: new google.maps.LatLng(lat, lng),
+    zoom: defaultZoom,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    scrollwheel: false,
+    scaleControl: false
+  };
+  var map = new google.maps.Map(document.getElementById('map'),mapOptions);
+  // Im Centering the map in Bangkok
+  // '13.731547730050778'
+  // '100.56798934936523'
+  var mylat = document.getElementById("location_lat").value;
+  var mylng = document.getElementById("location_lng").value;
+  if (jQuery.isEmptyObject(mylat)) {
+    var mylat = lat;
+    var mylng = lng;
+  };
+  // set center point of map to be of the Marker or the Default City
+  var centrePoint = new google.maps.LatLng(mylat, mylng);
+  map.setCenter(centrePoint);
+  map.setZoom(defaultZoom);
+  // add a draggable marker
+  marker = new google.maps.Marker({
+    map:map,
+    draggable: false,
+    animation: google.maps.Animation.DROP,
+    position: centrePoint
   });
-
-  var defaultBounds = new google.maps.LatLngBounds(
-      new google.maps.LatLng(40.7127, 74.0059),
-      new google.maps.LatLng(40.7637, 74.0959));
-  map.fitBounds(defaultBounds);
-
-  // Create the search box and link it to the UI element.
-  var input = /** @type {HTMLInputElement} */(
-      document.getElementById('pac-input'));
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  var searchBox = new google.maps.places.SearchBox(
-    /** @type {HTMLInputElement} */(input));
-
-  // Listen for the event fired when the user selects an item from the
-  // pick list. Retrieve the matching places for that item.
-  google.maps.event.addListener(searchBox, 'places_changed', function() {
-    var places = searchBox.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-    for (var i = 0, marker; marker = markers[i]; i++) {
-      marker.setMap(null);
-    }
-
-    // For each place, get the icon, place name, and location.
-    markers = [];
-    var bounds = new google.maps.LatLngBounds();
-    for (var i = 0, place; place = places[i]; i++) {
-      var image = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
-      // Create a marker for each place.
-      var marker = new google.maps.Marker({
-        map: map,
-        icon: image,
-        title: place.name,
-        position: place.geometry.location
-      });
-
-      markers.push(marker);
-
-      bounds.extend(place.geometry.location);
-    }
-
-    map.fitBounds(bounds);
-  });
-
-  // Bias the SearchBox results towards places that are within the bounds of the
-  // current map's viewport.
-  google.maps.event.addListener(map, 'bounds_changed', function() {
-    var bounds = map.getBounds();
-    searchBox.setBounds(bounds);
-  });
-}
+  // Set marker image
+  var defaultImage = new google.maps.MarkerImage(
+      "http://maps.gstatic.com/mapfiles/place_api/icons/geocode-71.png",
+      new google.maps.Size(71, 71),
+      new google.maps.Point(0, 0),
+      new google.maps.Point(17, 34),
+      new google.maps.Size(35, 35)
+  );
+  marker.setIcon(defaultImage);
 })
