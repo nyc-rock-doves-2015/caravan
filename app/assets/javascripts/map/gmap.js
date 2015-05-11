79b9a99c116f3c209a9a4ed04d90114f22f6612b
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-11 09:25:30 -0400

    Add in google places css and javascript

--- app/assets/javascripts/map/gmap.js
+++ app/assets/javascripts/map/gmap.js
@@ -1,8 +1,78 @@
- $(document).ready(function(){
- handler = Gmaps.build('Google');
-  handler.buildMap({ provider: {
-  }, internal: {id: 'map'}}, function(){
-    // markers = handler.addMarkers(#{raw @hash.to_json});
-    handler.fitMapToBounds();
+//  $(document).ready(function(){
+//  handler = Gmaps.build('Google');
+//   handler.buildMap({ provider: {
+//   }, internal: {id: 'map'}}, function(){
+//     // markers = handler.addMarkers(#{raw @hash.to_json});
+//     handler.fitMapToBounds();
+//   });
+// })
+
+$(document).ready(function(){
+ function initialize() {
+
+  var markers = [];
+  var map = new google.maps.Map(document.getElementById('map-canvas'), {
+    mapTypeId: google.maps.MapTypeId.ROADMAP
   });
+
+  var defaultBounds = new google.maps.LatLngBounds(
+      new google.maps.LatLng(40.7127, 74.0059),
+      new google.maps.LatLng(40.7637, 74.0959));
+  map.fitBounds(defaultBounds);
+
+  // Create the search box and link it to the UI element.
+  var input = /** @type {HTMLInputElement} */(
+      document.getElementById('pac-input'));
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  var searchBox = new google.maps.places.SearchBox(
+    /** @type {HTMLInputElement} */(input));
+
+  // Listen for the event fired when the user selects an item from the
+  // pick list. Retrieve the matching places for that item.
+  google.maps.event.addListener(searchBox, 'places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+    for (var i = 0, marker; marker = markers[i]; i++) {
+      marker.setMap(null);
+    }
+
+    // For each place, get the icon, place name, and location.
+    markers = [];
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0, place; place = places[i]; i++) {
+      var image = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      var marker = new google.maps.Marker({
+        map: map,
+        icon: image,
+        title: place.name,
+        position: place.geometry.location
+      });
+
+      markers.push(marker);
+
+      bounds.extend(place.geometry.location);
+    }
+
+    map.fitBounds(bounds);
+  });
+
+  // Bias the SearchBox results towards places that are within the bounds of the
+  // current map's viewport.
+  google.maps.event.addListener(map, 'bounds_changed', function() {
+    var bounds = map.getBounds();
+    searchBox.setBounds(bounds);
+  });
+}
 })
