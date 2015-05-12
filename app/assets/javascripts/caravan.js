37b2932afc8444a140391f4d5bdc2f5b49a4b47f
Charles Green <Charles.Green88@gmail.com>
2015-05-12 15:52:08 -0400

    add button id's to trip creation form

--- app/assets/javascripts/caravan.js
+++ app/assets/javascripts/caravan.js
@@ -1,28 +1,7 @@
-// $(document).ready(function() {
-//   $(window).keydown(function(event){
-//     if(event.keyCode == 13) {
-//     	event.preventDefault();
-//     	return false
-//     // 	var target = event.target
-//     // 	if (target = 'origin_address[address_string]'){
-//     // 		$("#button-1").click();
-//     // 		console.log("text-field-2");
-//     // 	} else if(target = 'destination_address[address_string]'){
-//     // 		$("#button-2").click();
-//     // 		console.log("clicked num 2");
-//     // 	} else if (target = "text-field-3"){
-//     // 		$("#button-3").click()
-//     // 	}
-//     //   // event.preventDefault();
-//     //   // return false;
-//     // }
-//   });
-// });
-
 $(document).ready(function() {
   $('#creation-form').on('keydown', 'input', function(event) {
     if(event.keyCode == 13) {
-      event.preventDefault();
+    	event.preventDefault();
       var $target = $(event.target);
       console.log($target.attr('id'));
     	if ($target.attr('id') == 'origin_address_address_string'){
@@ -31,7 +10,7 @@ $(document).ready(function() {
     	} else if($target.attr('id') == 'destination_address_address_string'){
     		$("#button-2").click();
     		console.log("text-field-2");
-    	} else if ($target.attr('id') == "text-field-3"){
+    	} else if ($target.attr('id') == "text-field-3") {
     		$("#button-3").click()
     	}
     }
