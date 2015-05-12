0cab6a6e31249ccb6a39ac2af39561957f752224
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-12 17:17:18 -0400

    Add ids to tabbed links on use profile

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -7,12 +7,27 @@ $(document).ready(function() {
       url: $details.context.action,
       type: 'GET',
       }).done(function (response) {
-        console.log(response)
           $('#sectionA').children().hide();
           $('#sectionA').append(response);
       }).fail(function (response) {
-
+        alert("Can Not Render Your Parcels Due to Error")
       })
     });
+
+   $('.details').on('click', function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      var $details = $(event.target.parentElement);
+      $.ajax({
+        url: $details.context.action,
+        type: 'GET',
+        }).done(function (response) {
+          console.log(response)
+            $('#sectionA').children().hide();
+            $('#sectionA').append(response);
+        }).fail(function (response) {
+
+        })
+      });
 
   });
