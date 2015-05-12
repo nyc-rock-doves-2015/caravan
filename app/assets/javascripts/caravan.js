1ddf83c0d858469130fa9428c78d20810dcda0c8
Charles Green <Charles.Green88@gmail.com>
2015-05-12 15:21:53 -0400

    prevent form submission on enter

---
+++ app/assets/javascripts/caravan.js
@@ -0,0 +1,28 @@
+// $(document).ready(function() {
+//   $(window).keydown(function(event){
+//     if(event.keyCode == 13) {
+//     	event.preventDefault();
+//     	return false
+//     // 	var target = event.target
+//     // 	if (target = 'origin_address[address_string]'){
+//     // 		$("#button-1").click();
+//     // 		console.log("text-field-2");
+//     // 	} else if(target = 'destination_address[address_string]'){
+//     // 		$("#button-2").click();
+//     // 		console.log("clicked num 2");
+//     // 	} else if (target = "text-field-3"){
+//     // 		$("#button-3").click()
+//     // 	}
+//     //   // event.preventDefault();
+//     //   // return false;
+//     // }
+//   });
+// });
+
+$(document).ready(function() {
+  $(window).keydown(function(event){
+    if(event.keyCode == 13) {
+      event.preventDefault();
+    }
+  });
+});
