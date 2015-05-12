aaacd71267c3ef42572d222d5d91e52e7553127b
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-12 17:33:33 -0400

    Unhide parcel summary collection on profile page after leaving detail page

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -14,20 +14,9 @@ $(document).ready(function() {
       })
     });
 
-   $('.details').on('click', function(event) {
-      event.preventDefault();
-      event.stopPropagation();
-      var $details = $(event.target.parentElement);
-      $.ajax({
-        url: $details.context.action,
-        type: 'GET',
-        }).done(function (response) {
-          console.log(response)
-            $('#sectionA').children().hide();
-            $('#sectionA').append(response);
-        }).fail(function (response) {
-
-        })
-      });
+   $('#parcels_tab').on('click', function(event) {
+      $('#sectionA').children().show();
+      $('.parcel_show').hide();
+    });
 
   });
