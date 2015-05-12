3230d7600ccfa0a631de0b818ba18df78bbf41eb
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-12 16:11:45 -0400

    Ajax for partial detail partial render on user profile

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,31 +1,16 @@
 $(document).ready(function() {
-  $.ajax({
-    method: "POST",
-    url: "some.php",
-    data: { name: "John", location: "Boston" }
-  })
-    .done(function( msg ) {
-      alert( "Data Saved: " + msg );
-    });
-    $.ajax
-  ><%= button_to 'details', parcel_path(parcel), class: "btn btn-primary nextBtn btn-xs", method: 'get'%></div>
-
-})
-
- $('#details').on('submit', function(event) {
+ $('.details').on('click', function(event) {
     event.preventDefault();
     event.stopPropagation();
-    var $details = $(event.target);
-
+    var $details = $(event.target.parentElement);
     $.ajax({
-      url: $details.attr('action'),
+      url: $details.context.action,
       type: 'GET',
-    }).done(function (response) {
-      console.log(response)
-    $('sectionA').closest('#new_question').html(response)
-      $question_form.hide;
-    }).fail(function (response) {
-
-
+      }).done(function (response) {
+        console.log(response)
+          $('#sectionA').html(response)
+      }).fail(function (response) {
+        alert("Error")
+      })
     });
   });
