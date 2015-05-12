f25316ab1275ece9a28c60bda8ce17bc81c4e99c
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-12 14:47:50 -0400

    Add ajax to parcel.js for parcel details view

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,1 +1,31 @@
-profile.js
+$(document).ready(function() {
+  $.ajax({
+    method: "POST",
+    url: "some.php",
+    data: { name: "John", location: "Boston" }
+  })
+    .done(function( msg ) {
+      alert( "Data Saved: " + msg );
+    });
+    $.ajax
+  ><%= button_to 'details', parcel_path(parcel), class: "btn btn-primary nextBtn btn-xs", method: 'get'%></div>
+
+})
+
+ $('#details').on('submit', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    var $details = $(event.target);
+
+    $.ajax({
+      url: $details.attr('action'),
+      type: 'GET',
+    }).done(function (response) {
+      console.log(response)
+    $('sectionA').closest('#new_question').html(response)
+      $question_form.hide;
+    }).fail(function (response) {
+
+
+    });
+  });
