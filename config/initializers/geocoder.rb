fd48dfb22badac29e9405b26262f33d49604ca3f
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-12 12:14:33 -0400

    Change geocoder config to lookup yandex

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -2,7 +2,7 @@ Geocoder.configure(
 
   # geocoding service (see below for supported options):
   # :lookup => :geocoder_ca,
-  :lookup => :geocoder_us,
+  :lookup => :yandex,
 
   # geocoding service request timeout, in seconds (default 3):
   :timeout => 5,
