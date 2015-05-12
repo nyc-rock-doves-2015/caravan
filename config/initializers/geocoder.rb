2b1c17081d771939873f98320d75561bcc26e1f1
bicyclethief <bicyclethief@gmail.com>
2015-05-12 12:07:42 -0400

    Switch geocode lookup service

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,7 +1,8 @@
 Geocoder.configure(
 
   # geocoding service (see below for supported options):
-  :lookup => :geocoder_ca,
+  # :lookup => :geocoder_ca,
+  :lookup => :geocoder_us,
 
   # geocoding service request timeout, in seconds (default 3):
   :timeout => 5,
