777efa753ae219df9380ac65823dcd005bfc2a08
kennygeiler <kennethlgeiler@gmail.com>
2015-05-12 14:51:30 -0400

    delete gmaps dependency

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,6 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require gmaps/google
-//= require underscore-min
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
