b60e8746b4e2ad5669d9469101d00c4b7a8f5f0a
Charles Green <Charles.Green88@gmail.com>
2015-05-12 15:47:59 -0400

    add jquery enter functionality on form

--- app/assets/javascripts/caravan.js
+++ app/assets/javascripts/caravan.js
@@ -23,6 +23,17 @@ $(document).ready(function() {
   $('#creation-form').on('keydown', 'input', function(event) {
     if(event.keyCode == 13) {
       event.preventDefault();
+      var $target = $(event.target);
+      console.log($target.attr('id'));
+    	if ($target.attr('id') == 'origin_address_address_string'){
+    		$("#button-1").click();
+    		console.log("text-field-1");
+    	} else if($target.attr('id') == 'destination_address_address_string'){
+    		$("#button-2").click();
+    		console.log("text-field-2");
+    	} else if ($target.attr('id') == "text-field-3"){
+    		$("#button-3").click()
+    	}
     }
   });
 });
