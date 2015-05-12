7b80922e6710651b5960ef80adf70d222ed9c38e
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-12 19:36:24 -0400

    Move single trip display from single to show, set up ajax event for trip details

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -19,9 +19,24 @@ $(document).ready(function() {
       $('.parcel_show').hide();
     });
 
-   $('.trips_tab').on('click', function(event) {
-      $('.nav-tabs:eq(1)').removeClass('active')
-      $('.nav-tabs:eq(2)').addClass('active')
+    $('.trip_details').on('click', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    var $details = $(event.target.parentElement);
+    $.ajax({
+      url: $details.context.action,
+      type: 'GET',
+      }).done(function (response) {
+          $('#sectionB').children().hide();
+          $('#sectionB').append(response);
+      }).fail(function (response) {
+        alert("Can Not Render Your Trips Due to Error")
+      })
+    });
+
+     $('.trips_tab').on('click', function(event) {
+      $('#sectionB').children().show();
+      $('.trip_show').hide();
     });
 
   });
