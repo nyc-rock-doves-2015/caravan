fa60fb8c93473db609529d0aadbe56fcc0e85b6b
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-13 19:09:55 -0400

    Eliminate console logs in js files

--- app/assets/javascripts/caravan.js
+++ app/assets/javascripts/caravan.js
@@ -3,13 +3,10 @@ $(document).ready(function() {
     if(event.keyCode == 13) {
     	event.preventDefault();
       var $target = $(event.target);
-      console.log($target.attr('id'));
     	if ($target.attr('id') == 'origin_address_address_string'){
     		$("#button-1").click();
-    		console.log("text-field-1");
     	} else if($target.attr('id') == 'destination_address_address_string'){
     		$("#button-2").click();
-    		console.log("text-field-2");
     	} else if ($target.attr('id') == "text-field-3") {
     		$("#button-3").click()
     	}
