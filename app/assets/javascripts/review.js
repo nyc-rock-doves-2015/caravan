a0451556e4e40adfdebc81a0d565a52674019676
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-13 16:47:42 -0400

    Add review class to review link, add review js

---
+++ app/assets/javascripts/review.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+ $('#review').on('click', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    var $details = $(event.target);
+    console.log($details[0].href)
+    $.ajax({
+      url: $details[0].href,
+      type: 'GET',
+      }).done(function (response) {
+          $('.profile-content').children().hide();
+          $('.profile-content').append(response);
+      }).fail(function (response) {
+        alert("Can Not Render Your Parcels Due to Error")
+      })
