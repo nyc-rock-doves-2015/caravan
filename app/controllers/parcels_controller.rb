8413f5a3d2f0e830f0f145a23448380580387648
grantziolkowski <grant.ziolkowski@gmail.com>
2015-05-14 17:13:07 -0400

    Remove hidden params from parcel index form

--- app/controllers/parcels_controller.rb
+++ app/controllers/parcels_controller.rb
@@ -54,6 +54,7 @@ class ParcelsController < ApplicationController
 
   def update
     @parcel = Parcel.find(params[:id])
+    p params
     @origin_address = @parcel.origin_address
     @destination_address = @parcel.destination_address
     if @parcel.update(parcel_params) && @origin_address.update(origin_address_params) && @destination_address.update(destination_address_params)
